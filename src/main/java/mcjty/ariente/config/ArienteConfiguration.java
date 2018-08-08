047cf4eb86d83315a2bdd87ee6d284d8e04a9a0e
McJty <mcjty1@gmail.com>
2018-08-08 20:56:25 +0200

    New type of city

--- src/main/java/mcjty/ariente/config/ArienteConfiguration.java
+++ src/main/java/mcjty/ariente/config/ArienteConfiguration.java
@@ -4,6 +4,7 @@ public class ArienteConfiguration {
 
     public static String[] ASSETS = new String[] {
             "/assets/ariente/citydata/city1_1.json",
+            "/assets/ariente/citydata/city1_2.json",
             "/assets/ariente/citydata/city2_1.json",
             "$ariente/userassets.json"
     };
