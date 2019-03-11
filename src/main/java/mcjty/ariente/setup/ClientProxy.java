66fa59522d83c4a6caf13580819907928c29f7c0
McJty <mcjty1@gmail.com>
2019-03-11 11:41:18 +0100

    More cleanups and removal of unneeded things

--- src/main/java/mcjty/ariente/setup/ClientProxy.java
+++ src/main/java/mcjty/ariente/setup/ClientProxy.java
@@ -6,7 +6,6 @@ import mcjty.ariente.bindings.KeyBindings;
 import mcjty.ariente.bindings.KeyInputHandler;
 import mcjty.ariente.blocks.ModBlocks;
 import mcjty.ariente.cables.BakedModelLoader;
-import mcjty.lib.McJtyLibClient;
 import mcjty.lib.setup.DefaultClientProxy;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
@@ -24,7 +23,6 @@ public class ClientProxy extends DefaultClientProxy {
         super.preInit(e);
         MinecraftForge.EVENT_BUS.register(this);
         MinecraftForge.EVENT_BUS.register(new ClientForgeEventHandlers());
-        McJtyLibClient.preInit(e);
         OBJLoader.INSTANCE.addDomain(Ariente.MODID);
         ModelLoaderRegistry.registerLoader(new BakedModelLoader());
     }
