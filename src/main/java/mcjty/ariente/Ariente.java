3b295271dd8fe7f5175aec0edf00a43797e76978
McJty <mcjty1@gmail.com>
2019-05-05 14:03:10 +0200

    Version bumps

--- src/main/java/mcjty/ariente/Ariente.java
+++ src/main/java/mcjty/ariente/Ariente.java
@@ -30,8 +30,8 @@ public class Ariente implements ModBase, IArienteMod {
     public static final String MODNAME = "Ariente";
     public static final String VERSION = "0.0.20-alpha";
     public static final String MIN_FORGE11_VER = "14.23.3.2694";
-    public static final String MIN_MCJTYLIB_VER = "3.5.0";
-    public static final String MIN_HOLOGUI_VER = "0.0.6-beta";
+    public static final String MIN_MCJTYLIB_VER = "3.5.1";
+    public static final String MIN_HOLOGUI_VER = "0.0.7-beta";
 
     @SidedProxy(clientSide = "mcjty.ariente.setup.ClientProxy", serverSide = "mcjty.ariente.setup.ServerProxy")
     public static IProxy proxy;
