2751e31d7cfcd6baa852d1e613dd6f40999ac2b8
McJty <mcjty1@gmail.com>
2019-05-08 05:04:12 +0200

    Fixed another problem with generated flux levitators crashing

--- src/main/java/mcjty/ariente/api/IFluxLevitatorEntity.java
+++ src/main/java/mcjty/ariente/api/IFluxLevitatorEntity.java
@@ -4,8 +4,6 @@ import net.minecraft.util.math.BlockPos;
 
 public interface IFluxLevitatorEntity {
 
-    BlockPos getPosition();
-
     void changeSpeed(int speed);
 
     BlockPos getDesiredDestination();
