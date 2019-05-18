f479e047deb0951007a679f74b24021b0a05ba4d
McJty <mcjty1@gmail.com>
2019-05-18 19:40:12 +0200

    There was no way to make negarite and posirite dust from the ores. Oops!

--- src/main/java/mcjty/ariente/ModCrafting.java
+++ src/main/java/mcjty/ariente/ModCrafting.java
@@ -11,5 +11,7 @@ public final class ModCrafting {
         GameRegistry.addSmelting(ModBlocks.manganeseore, new ItemStack(ModItems.manganeseIngot, 1), 0.0f);
         GameRegistry.addSmelting(ModBlocks.platinumore, new ItemStack(ModItems.platinumIngot, 1), 0.0f);
         GameRegistry.addSmelting(ModBlocks.silverore, new ItemStack(ModItems.silverIngot, 1), 0.0f);
+        GameRegistry.addSmelting(ModBlocks.negarite, new ItemStack(ModItems.negariteDust, 4), 0.0f);
+        GameRegistry.addSmelting(ModBlocks.posirite, new ItemStack(ModItems.posiriteDust, 4), 0.0f);
     }
 }
