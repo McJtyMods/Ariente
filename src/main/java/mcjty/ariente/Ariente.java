7104b811df556a265759a9f1d91b28a28b346efc
McJty <mcjty1@gmail.com>
2019-08-11 06:49:16 +0200

    0.0.31-alpha

--- src/main/java/mcjty/ariente/Ariente.java
+++ src/main/java/mcjty/ariente/Ariente.java
@@ -28,7 +28,7 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 public class Ariente implements ModBase, IArienteMod {
     public static final String MODID = "ariente";
     public static final String MODNAME = "Ariente";
-    public static final String VERSION = "0.0.30-alpha";
+    public static final String VERSION = "0.0.31-alpha";
     public static final String MIN_FORGE11_VER = "14.23.3.2694";
     public static final String MIN_MCJTYLIB_VER = "3.5.3";
     public static final String MIN_HOLOGUI_VER = "0.0.9-beta";
